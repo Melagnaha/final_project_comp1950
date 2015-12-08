c55637c60603c4efdb6f621cac7689cf5278bc9b
Vaidas <vaidas@makselis.ca>
2015-12-08 00:30:55 -0800

    proto styles update

---
+++ protosite/js/protosite.js
@@ -0,0 +1,34 @@
+/**
+ * Created by rojus on 15-12-07.
+ */
+
+
+$(document).ready(function(){
+
+    // functions
+
+    function loadQuiz (e){
+
+        var quiz = $(this).parent().siblings('section.quizWrapper');
+        e.preventDefault();
+        e.stopPropagation(); // to stop parent reacting to child events
+        quiz.fadeToggle(300);
+    }
+
+    function loadClass(e){
+
+        var main = $(this).children('main');
+        e.preventDefault();
+
+        main.addClass('open');
+        main.fadeToggle(300);
+    }
+
+    // events
+
+    $('.quiz').click(loadQuiz);
+    $('section.active').click(loadClass);
+    $('main').click(function(e){e.stopPropagation();});
+    $('.quizWrapper').click(function(e){e.stopPropagation();});
+});
+
